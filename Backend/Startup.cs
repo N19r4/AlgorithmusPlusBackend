814d8bfbb09a40167da724587235293b66a94e9e
Kinga Grabarczyk <kinga.grabarczyk@euvic.pl>
2023-11-19 17:09:04 +0100

    Dodaj pliki projektów.

---
+++ Backend/Startup.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Backend
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(builder =>
+                {
+                    builder.WithOrigins("http://localhost:8081")
+                           .AllowAnyHeader()
+                           .AllowAnyMethod();
+                });
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseCors();
+        }
+
+    }
+}
