a86fcbd2a69a7c0b67fc2f7962f69a9b125242ae
pardonmyfriend <eva.kedziera@gmail.com>
2023-12-04 09:08:30 +0100

    Endpoint upload test function

--- Backend/Controllers/WeatherForecastController.cs
+++ Backend/Controllers/WeatherForecastController.cs
@@ -1,5 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
-
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
 namespace Backend.Controllers
 {
     [ApiController]
@@ -30,21 +36,38 @@ namespace Backend.Controllers
             .ToArray();
         }
 
-        [HttpGet(Name = "GetTestFunction")]
-        [ProducesResponseType(200, Type = typeof(TestFunction))]
-        [ProducesResponseType(400)]
-        public IActionResult Get(int id)
+        [HttpPost("PostTestFunction")]
+        public IActionResult UploadTestFunction(TestFunction testFunction)
         {
-            TestFunction testFunction = new TestFunction
-            {
-                Name = "SampleTestFunction",
-                DLLPath = "SampleDLLPath",
-                Params = new string[] { "Param1", "Param2" }
-            };
+            //TestFunction testFunction = new TestFunction
+            //{
+            //    Name = "SampleTestFunction",
+            //    DLLPath = "SampleDLLPath",
+            //    Params = new string[] { "Param1", "Param2" }
+            //}
+
 
             if (testFunction == null)
             {
                 return NotFound();
+            }
+
+            string testFunctionsFolder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "TestFunctions");
+            Directory.CreateDirectory(testFunctionsFolder);
+
+            // Zapisanie DLL z funkcj¹ testow¹ do katalogu TestFunctions
+            string testFunctionDLL = $@"{testFunction.DLLPath}";
+            string testFunctionPath = Path.Combine(testFunctionsFolder, Path.GetFileName(testFunctionDLL));
+
+            if (File.Exists(testFunctionPath))
+            {
+                File.Copy(testFunctionDLL, testFunctionPath, true);
+                Console.WriteLine($"Plik {Path.GetFileName(testFunctionPath)} zosta³ nadpisany.");
+            }
+            else
+            {
+                File.Copy(testFunctionDLL, testFunctionPath);
+                Console.WriteLine($"Plik {Path.GetFileName(testFunctionPath)} zosta³ skopiowany.");
             }
 
             return Ok(testFunction);
