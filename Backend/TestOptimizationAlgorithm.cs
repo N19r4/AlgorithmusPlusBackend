c3b08bbab5f4b3323c97fe980accf803ea219d72
pardonmyfriend <eva.kedziera@gmail.com>
2023-12-19 01:27:33 +0100

    Dostosowanie do wymagań, poprawa testowania i uruchamiania algorytmu

--- Backend/TestOptimizationAlgorithm.cs
+++ Backend/TestOptimizationAlgorithm.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using Backend;
 using CsvHelper;
 using CsvHelper.Configuration;
 
@@ -15,11 +16,18 @@ namespace TestDLL
 {
     public class TestOptimizationAlgorithm
     {
-        public static void RunTests(int[] T, int[] N, Type optimizationAlgorithmType, List<object> testFunctions, Type delegateFunction)
+        public static void RunTests(List<object> testFunctions, object optimizationAlgorithm, List<double[]> paramsList, Type delegateFunction)
         {
             string reportFilePath = "report.csv";
 
             List<TestResult> testResults = new List<TestResult>();
+
+            // tutaj tworze tablice kombinacji parametrów przygotowane już do testów
+            List<double[]> paramsListForTest = GetParamsList(paramsList);
+
+            var optimizationAlgorithmName = optimizationAlgorithm.GetType().GetProperty("Name").GetValue(optimizationAlgorithm);
+            var numberOfEvaluationFitnessFunction = (int)optimizationAlgorithm.GetType().GetProperty("NumberOfEvaluationFitnessFunction").GetValue(optimizationAlgorithm);
+            var solve = optimizationAlgorithm.GetType().GetMethod("Solve");
 
             foreach (var testFunction in testFunctions)
             {
@@ -31,130 +39,113 @@ namespace TestDLL
                 var calculateMethodInfo = testFunction.GetType().GetMethod("Calculate");
                 var calculate = Delegate.CreateDelegate(delegateFunction, testFunction, calculateMethodInfo);
 
-                foreach (var t in T)
+                foreach (var parameters in paramsListForTest)
                 {
-                    foreach (var n in N)
+                    double[,] bestData = new double[dim + 1, 10];
+                    string executionTime = "";
+
+                    for (int i = 0; i < 10; i++)
                     {
-                        var optimizationAlgorithm = Activator.CreateInstance(
-                            optimizationAlgorithmType,
-                            n,
-                            t,
-                            calculate,
-                            dim,
-                            xmin,
-                            xmax
-                            );
+                        var watch = System.Diagnostics.Stopwatch.StartNew();
+                        solve.Invoke(optimizationAlgorithm, new object[] { });
+                        watch.Stop();
 
-                        var optimizationAlgorithmName = optimizationAlgorithm.GetType().GetProperty("Name").GetValue(optimizationAlgorithm);
-                        var numberOfEvaluationFitnessFunction = (int)optimizationAlgorithm.GetType().GetProperty("NumberOfEvaluationFitnessFunction").GetValue(optimizationAlgorithm);
-                        var solve = optimizationAlgorithm.GetType().GetMethod("Solve");
+                        var elapsedMs = watch.ElapsedMilliseconds;
+                        executionTime = elapsedMs.ToString();
 
-                        double[,] bestData = new double[dim + 1, 10];
-                        string executionTime = "";
+                        var XBest = (double[])optimizationAlgorithm.GetType().GetProperty("XBest").GetValue(optimizationAlgorithm);
+                        var FBest = (double)optimizationAlgorithm.GetType().GetProperty("XBest").GetValue(optimizationAlgorithm);
+                        numberOfEvaluationFitnessFunction = (int)optimizationAlgorithm.GetType().GetProperty("NumberOfEvaluationFitnessFunction").GetValue(optimizationAlgorithm);
 
-                        for (int i = 0; i < 10; i++)
+                        for (int j = 0; j < dim; j++)
                         {
-                            //log progress
-                            // Console.WriteLine($"Algorytm: {optimizationAlgorithmName}, Funkcja testowa: {testFunctionName}, Liczba szukanych parametrów: {dim}, Liczba iteracji: {t}, Rozmiar populacji: {n}, Iteracja: {i + 1}");
-                            var watch = System.Diagnostics.Stopwatch.StartNew();
-                            var FBest = (double)solve.Invoke(optimizationAlgorithm, new object[] {});
-                            watch.Stop();
-
-                            var elapsedMs = watch.ElapsedMilliseconds;
-                            executionTime = elapsedMs.ToString();
-
-                            var XBest = (double[])optimizationAlgorithm.GetType().GetProperty("XBest").GetValue(optimizationAlgorithm);
-                            numberOfEvaluationFitnessFunction = (int)optimizationAlgorithm.GetType().GetProperty("NumberOfEvaluationFitnessFunction").GetValue(optimizationAlgorithm);
-
-                            for (int j = 0; j < dim; j++)
-                            {
-                                bestData[j, i] = XBest[j];
-                            }
-
-                            bestData[dim, i] = FBest;
+                            bestData[j, i] = XBest[j];
                         }
 
-                        double minFunction = bestData[dim, 0];
-                        int minFunction_index = 0;
-                        for (int i = 1; i < 10; i++)
-                        {
-                            if (bestData[dim, i] < minFunction)
-                            {
-                                minFunction = bestData[dim, i];
-                                minFunction_index = i;
-                            }
-                        }
-
-                        double[] allMinFunction = new double[10];
-
-                        for (int i = 0; i < 10; i++)
-                        {
-                            allMinFunction[i] = bestData[dim, i];
-                        }
-
-                        double avgForFunction = allMinFunction.Average();
-                        double sumForFunction = allMinFunction.Sum(x => Math.Pow(x - avgForFunction, 2));
-                        double stdDevForFunction = Math.Sqrt(sumForFunction / 10);
-                        double varCoeffForFunction = 0;
-                        if (stdDevForFunction != 0)
-                            varCoeffForFunction = (stdDevForFunction / avgForFunction) * 100;
-                        else
-                            varCoeffForFunction = 0;
-
-
-                        double[] minParameters = new double[dim];
-                        for (int i = 0; i < dim; i++)
-                            minParameters[i] = bestData[i, minFunction_index];
-
-                        string str_minParameters = "(" + string.Join(", ", minParameters.Select(d => d.ToString("F5", CultureInfo.InvariantCulture))) + ")";
-
-                        double[] stdDevForParameters = new double[dim];
-                        double[] varCoeffForParameters = new double[dim];
-
-                        for (int i = 0; i < dim; i++)
-                        {
-                            double[] parameters = new double[10];
-                            for (int j = 0; j < 10; j++)
-                            {
-                                parameters[j] = bestData[i, j];
-                            }
-
-                            double avg = parameters.Average();
-                            double sum = parameters.Sum(x => Math.Pow(x - avg, 2));
-                            double stdDev = Math.Sqrt(sum / 10);
-                            double varCoeff = 0;
-                            if (stdDev != 0)
-                                varCoeff = (stdDev / avg) * 100;
-                            else
-                                varCoeff = 0;
-
-                            stdDevForParameters[i] = stdDev;
-                            varCoeffForParameters[i] = varCoeff;
-                        }
-
-                        string str_stdDevForParameters = "(" + string.Join(", ", stdDevForParameters.Select(d => d.ToString("F5", CultureInfo.InvariantCulture))) + ")";
-                        string str_varCoeffForParameters = "(" + string.Join(", ", varCoeffForParameters.Select(d => d.ToString("F5", CultureInfo.InvariantCulture))) + ")";
-
-                        TestResult testResult = new TestResult
-                        {
-                            Algorytm = optimizationAlgorithmName.ToString(),
-                            FunkcjaTestowa = testFunctionName.ToString(),
-                            LiczbaSzukanychParametrów = dim,
-                            LiczbaIteracji = t,
-                            RozmiarPopulacji = n,
-                            ZnalezioneMinimum = str_minParameters,
-                            OdchylenieStandardowePoszukiwanychParametrów = str_stdDevForParameters,
-                            WartośćFunkcjiCelu = minFunction.ToString("F5", CultureInfo.InvariantCulture),
-                            OdchylenieStandardoweWartościFunkcjiCelu = stdDevForFunction.ToString("F5", CultureInfo.InvariantCulture),
-                            LiczbaWywołańFunkcjiCelu = numberOfEvaluationFitnessFunction,
-                            CzasEgzekucji = executionTime
-                        };
-
-                        testResults.Add(testResult);
+                        bestData[dim, i] = FBest;
                     }
+
+                    double minFunction = bestData[dim, 0];
+                    int minFunction_index = 0;
+                    for (int i = 1; i < 10; i++)
+                    {
+                        if (bestData[dim, i] < minFunction)
+                        {
+                            minFunction = bestData[dim, i];
+                            minFunction_index = i;
+                        }
+                    }
+
+                    double[] allMinFunction = new double[10];
+
+                    for (int i = 0; i < 10; i++)
+                    {
+                        allMinFunction[i] = bestData[dim, i];
+                    }
+
+                    double avgForFunction = allMinFunction.Average();
+                    double sumForFunction = allMinFunction.Sum(x => Math.Pow(x - avgForFunction, 2));
+                    double stdDevForFunction = Math.Sqrt(sumForFunction / 10);
+                    double varCoeffForFunction = 0;
+                    if (stdDevForFunction != 0)
+                        varCoeffForFunction = (stdDevForFunction / avgForFunction) * 100;
+                    else
+                        varCoeffForFunction = 0;
+
+
+                    double[] minParameters = new double[dim];
+                    for (int i = 0; i < dim; i++)
+                        minParameters[i] = bestData[i, minFunction_index];
+
+                    string str_minParameters = "(" + string.Join("; ", minParameters) + ")";
+
+                    double[] stdDevForParameters = new double[dim];
+                    double[] varCoeffForParameters = new double[dim];
+
+                    for (int i = 0; i < dim; i++)
+                    {
+                        double[] minimums = new double[10];
+                        for (int j = 0; j < 10; j++)
+                        {
+                            minimums[j] = bestData[i, j];
+                        }
+
+                        double avg = minimums.Average();
+                        double sum = minimums.Sum(x => Math.Pow(x - avg, 2));
+                        double stdDev = Math.Sqrt(sum / 10);
+                        double varCoeff = 0;
+                        if (stdDev != 0)
+                            varCoeff = (stdDev / avg) * 100;
+                        else
+                            varCoeff = 0;
+
+                        stdDevForParameters[i] = stdDev;
+                        varCoeffForParameters[i] = varCoeff;
+                    }
+
+                    string str_stdDevForParameters = "(" + string.Join("; ", stdDevForParameters) + ")";
+                    string str_varCoeffForParameters = "(" + string.Join("; ", varCoeffForParameters) + ")";
+
+                    TestResult testResult = new TestResult
+                    {
+                        Algorytm = optimizationAlgorithmName.ToString(),
+                        FunkcjaTestowa = testFunctionName.ToString(),
+                        LiczbaSzukanychParametrów = dim,
+                        LiczbaIteracji = (int)parameters[0],
+                        RozmiarPopulacji = (int)parameters[1],
+                        ZnalezioneMinimum = str_minParameters,
+                        OdchylenieStandardowePoszukiwanychParametrów = str_stdDevForParameters,
+                        WartośćFunkcjiCelu = minFunction.ToString("F5", CultureInfo.InvariantCulture),
+                        OdchylenieStandardoweWartościFunkcjiCelu = stdDevForFunction.ToString("F5", CultureInfo.InvariantCulture),
+                        LiczbaWywołańFunkcjiCelu = numberOfEvaluationFitnessFunction,
+                        CzasEgzekucji = executionTime
+                    };
+
+                    testResults.Add(testResult);
                 }
             }
 
+            // Zapisz CSV
             var config = new CsvConfiguration(new System.Globalization.CultureInfo("en-US"));
             using (var writer = new StreamWriter(reportFilePath))
             using (var csv = new CsvWriter(writer, config))
@@ -163,6 +154,30 @@ namespace TestDLL
             }
             //success:
             Console.WriteLine($"Zapisano wyniki do pliku: {reportFilePath}");
+        }
+
+        static List<double[]> GetParamsList(List<double[]> paramsList, int currentIndex = 0, double[] currentCombination = null)
+        {
+            if (currentCombination == null)
+                currentCombination = new double[paramsList.Count];
+
+            List<double[]> parameters = new List<double[]>();
+
+            if (currentIndex == paramsList.Count)
+            {
+                parameters.Add(currentCombination.ToArray());
+            }
+            else
+            {
+                foreach (var value in paramsList[currentIndex])
+                {
+                    currentCombination[currentIndex] = value;
+
+                    parameters.AddRange(GetParamsList(paramsList, currentIndex + 1, currentCombination));
+                }
+            }
+
+            return parameters;
         }
     }
 }
