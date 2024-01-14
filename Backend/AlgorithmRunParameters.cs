5c69f495b459c66c9daea457127c1e7096eaaa0b
Irek Kosek <irekkosek.IK@gmail.com>
2024-01-15 00:28:42 +0100

    auto detect best params and return them

--- Backend/AlgorithmRunParameters.cs
+++ Backend/AlgorithmRunParameters.cs
@@ -5,6 +5,8 @@ namespace Backend
         public string[] OptimizationAlgorithmNames { get; set; }
         public string[] TestFunctionNames { get; set; }
         public int Dim { get; set; }
-        public List<ParamForAlgorithm> paramsForAlgorithm { get; set; }
+        //map of params for each algorithm
+        public Dictionary<string, List<ParamForAlgorithm>> ParamsDict { get; set; }
+        // public List<ParamForAlgorithm> paramsForAlgorithm { get; set; }
     }
 }
